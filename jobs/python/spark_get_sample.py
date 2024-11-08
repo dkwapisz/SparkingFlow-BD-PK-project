28e4e3429d3939453b04cb885e620819d3107b93
dkwapisz <dkwapisz99@gmail.com>
2024-11-08 22:27:32 +0100

    Initial files for fork (not working)

--- jobs/python/spark_get_sample.py
+++ jobs/python/spark_get_sample.py
@@ -1,30 +1,25 @@
+import json
+
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.functions import rand
 from airflow.models import Variable
 
 # Initialize Spark session
 spark = SparkSession.builder.master('local').appName("GetSample").getOrCreate()
 
-# Define schema
-schema = StructType([
-    StructField("app_id", StringType(), True),
-    StructField("app_name", StringType(), True),
-    StructField("language", StringType(), True)
-])
+# Load CSV file with schema
+df = spark.read.option("header", "true").csv("dags/datasets/steam_reviews.csv")
 
-# Load CSV file
-df = (spark.read.format("csv")
-      .option("header", "true")
-      .load('/opt/airflow/dags/datasets/steam_reviews.csv')
-      .select("app_id", "app_name", "language"))
+# Select specific columns
+selected_columns_df = df.select("app_id", "app_name", "language")
 
-# Sample 1000 random rows
+# Sample approximately 1000 random rows
 sample_df = df.orderBy(rand()).limit(1000)
 
 # Convert DataFrame to JSON and save to Airflow Variable
-sample_data = sample_df.toJSON().collect()
-Variable.set("sample_data", sample_data)
+sample_data_json = sample_df.toJSON().collect()
+json_string = json.dumps(sample_data_json)
+Variable.set("sample_data", json_string)
 
 # Stop Spark session
 spark.stop()
