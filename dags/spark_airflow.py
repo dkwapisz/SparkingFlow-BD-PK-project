cffadedb58a2abd5b1c94603ea4f48ca8fa05a0f
dkwapisz <dkwapisz99@gmail.com>
2024-11-09 18:54:15 +0100

    Make partition job

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -6,7 +6,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 dag = DAG(
     dag_id="sparking_flow",
     default_args={
-        "owner": "Grupa C - Steam Reviews",
+        "owner": "Group C - Steam Reviews",
         "start_date": airflow.utils.dates.days_ago(1)
     },
     schedule_interval="@daily"
@@ -25,19 +25,19 @@ start = PythonOperator(
     dag=dag
 )
 
-python_job = SparkSubmitOperator(
-    task_id="get_sample_job",
+make_partition_job = SparkSubmitOperator(
+    task_id="make_partition",
     conn_id="spark-conn",
-    application="jobs/python/spark_get_sample.py",
+    application="jobs/python/spark_make_partition.py",
     dag=dag
 )
 
-print_job = SparkSubmitOperator(
-    task_id="print_sample_job",
-    conn_id="spark-conn",
-    application="jobs/python/spark_print_sample.py",
-    dag=dag
-)
+# print_job = SparkSubmitOperator(
+#     task_id="print_sample_job",
+#     conn_id="spark-conn",
+#     application="jobs/python/spark_print_sample.py",
+#     dag=dag
+# )
 
 end = PythonOperator(
     task_id="end",
@@ -45,4 +45,4 @@ end = PythonOperator(
     dag=dag
 )
 
-start >> health_check >> python_job >> print_job >> end
+start >> health_check >> make_partition_job >> end
