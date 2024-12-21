838af179042e9a3dd16beb1c33a3e4d4dc7c11b5
Krzysztof <krzysztof.karpala@gmail.com>
2024-12-21 19:10:08 +0100

    Poprawienie Dagów

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -7,99 +7,138 @@ source_path = "/opt/airflow/data/source/"
 bronze_path = "/opt/airflow/data/bronze/"
 silver_path = "/opt/airflow/data/silver/"
 
-dag = DAG(
-    dag_id="sparking_flow",
-    default_args={
-        "owner": "Group C - Steam Reviews",
-        "start_date": airflow.utils.dates.days_ago(1),
-    },
-)
 
-start = PythonOperator(
-    task_id="start", python_callable=lambda: print("Jobs started"), dag=dag
-)
+def create_dag_own(dag_id, job_type, translate: bool):
 
-health_check = SparkSubmitOperator(
-    task_id="health_check",
-    conn_id="spark-conn",
-    application="jobs/python/healthcheck_wordcount.py",
-    dag=dag,
-)
+    dag = DAG(
+        dag_id=dag_id,
+        default_args={
+            "owner": "Group C - Steam Reviews",
+            "start_date": airflow.utils.dates.days_ago(1),
+        },
+    )
 
-# load_sample_job = SparkSubmitOperator(
-#     task_id="load_sample",
-#     conn_id="spark-conn",
-#     application="jobs/python/load_sample.py",
-#     application_args=["--source_path", source_path,
-#                       "--num_partition", "80",
-#                       "--bronze_path", bronze_path],
-#     dag=dag
-# )
+    start = PythonOperator(
+        task_id="start", python_callable=lambda: print("Jobs started"), dag=dag
+    )
 
-load_games_to_bronze_job = SparkSubmitOperator(
-    task_id="load_games_to_bronze",
-    conn_id="spark-conn",
-    application="jobs/python/load_games_to_bronze.py",
-    application_args=["--source_path", source_path, "--bronze_path", bronze_path],
-    dag=dag,
-)
+    health_check = SparkSubmitOperator(
+        task_id="health_check",
+        conn_id="spark-conn",
+        application="jobs/python/healthcheck_wordcount.py",
+        dag=dag,
+    )
 
-partition_data_job = SparkSubmitOperator(
-    task_id="partition_data",
-    conn_id="spark-conn",
-    application="jobs/python/partition_data.py",
-    application_args=["--bronze_path", bronze_path, "--silver_path", silver_path],
-    dag=dag,
-)
+    if job_type == "sample":
+        load_sample_job = SparkSubmitOperator(
+            task_id="load_sample",
+            conn_id="spark-conn",
+            application="jobs/python/load_sample.py",
+            application_args=["--source_path", source_path,
+                              "--num_partition", "80",
+                              "--bronze_path", bronze_path],
+            dag=dag
+        )
 
-# translate_reviews_job = SparkSubmitOperator(
-#     task_id="translate_reviews",
-#     conn_id="spark-conn",
-#     application="jobs/python/translate_reviews.py",
-#     application_args=["--model_name", "/opt/airflow/data/source/opus-mt-mul-en",
-#                       "--silver_path", silver_path],
-#     dag=dag
-# )
+    elif job_type == "full":
+        load_full_job = SparkSubmitOperator(
+            task_id="load_full",
+            conn_id="spark-conn",
+            application="jobs/python/load_full.py",
+            application_args=["--source_path", source_path,
+                              "--num_partition", "200",
+                              "--bronze_path", bronze_path],
+            dag=dag
+        )
+
+    if job_type in ["sample", "full"]:
+        load_games_to_bronze_job = SparkSubmitOperator(
+            task_id="load_games_to_bronze",
+            conn_id="spark-conn",
+            application="jobs/python/load_games_to_bronze.py",
+            application_args=["--source_path", source_path, "--bronze_path", bronze_path],
+            dag=dag,
+        )
+
+    partition_data_job = SparkSubmitOperator(
+        task_id="partition_data",
+        conn_id="spark-conn",
+        application="jobs/python/partition_data.py",
+        application_args=["--bronze_path", bronze_path, "--silver_path", silver_path],
+        dag=dag,
+    )
+
+    if(translate):
+        translate_reviews_job = SparkSubmitOperator(
+            task_id="translate_reviews",
+            conn_id="spark-conn",
+            application="jobs/python/translate_reviews.py",
+            application_args=["--model_name", "/opt/airflow/data/source/opus-mt-mul-en",
+                            "--silver_path", silver_path],
+            dag=dag
+        )
 
 
-include_games_data_job = SparkSubmitOperator(
-    task_id="include_games_data",
-    conn_id="spark-conn",
-    application="jobs/python/include_games_data.py",
-    application_args=["--bronze_path", bronze_path, "--silver_path", silver_path],
-    dag=dag,
-)
+    include_games_data_job = SparkSubmitOperator(
+        task_id="include_games_data",
+        conn_id="spark-conn",
+        application="jobs/python/include_games_data.py",
+        application_args=["--bronze_path", bronze_path, "--silver_path", silver_path],
+        dag=dag,
+    )
 
-save_to_db_job = SparkSubmitOperator(
-    task_id="save_to_db",
-    conn_id="spark-conn",
-    application="jobs/python/save_to_db.py",
-    application_args=["--silver_path", silver_path],
-    dag=dag,
-    jars="/opt/airflow/jars/postgresql-42.2.29.jre7.jar",
-)
+    save_to_db_job = SparkSubmitOperator(
+        task_id="save_to_db",
+        conn_id="spark-conn",
+        application="jobs/python/save_to_db.py",
+        application_args=["--silver_path", silver_path],
+        dag=dag,
+        jars="/opt/airflow/jars/postgresql-42.2.29.jre7.jar",
+    )
 
-analytics_job = SparkSubmitOperator(
-    task_id="analytics",
-    conn_id="spark-conn",
-    application="jobs/python/analytics.py",
-    application_args=["--silver_path", silver_path],
-    dag=dag,
-    jars="/opt/airflow/jars/postgresql-42.2.29.jre7.jar",
-)
+    analytics_job = SparkSubmitOperator(
+        task_id="analytics",
+        conn_id="spark-conn",
+        application="jobs/python/analytics.py",
+        application_args=["--silver_path", silver_path],
+        dag=dag,
+        jars="/opt/airflow/jars/postgresql-42.2.29.jre7.jar",
+    )
 
-end = PythonOperator(
-    task_id="end", python_callable=lambda: print("Jobs completed successfully"), dag=dag
-)
+    end = PythonOperator(
+        task_id="end", python_callable=lambda: print("Jobs completed successfully"), dag=dag
+    )
 
-# start >> health_check >> load_sample_job >> load_games_to_bronze_job >> partition_data_job >> translate_reviews_job >> include_games_data_job >> save_to_db_job >> analytics_job >> end
-(
-    start
-    >> health_check
-    >> load_games_to_bronze_job
-    >> partition_data_job
-    >> include_games_data_job
-    >> save_to_db_job
-    >> analytics_job
-    >> end
-)
+    if job_type == "sample":
+        start >> health_check >> load_sample_job >> load_games_to_bronze_job
+        load_games_to_bronze_job >> partition_data_job
+
+    elif job_type == "full":
+        start >> health_check >> load_full_job >> load_games_to_bronze_job
+        load_games_to_bronze_job >> partition_data_job
+
+    else:
+        start >> health_check >> partition_data_job
+
+    if translate:
+        partition_data_job >> translate_reviews_job >> include_games_data_job
+    else:
+        partition_data_job >> include_games_data_job
+
+    include_games_data_job >> save_to_db_job >> analytics_job >> end
+
+    return dag
+
+sample_dag = create_dag_own("sample_dag", job_type="sample", translate=False)
+
+full_dag = create_dag_own("full_dag", job_type="full", translate=False)
+
+test_dag = create_dag_own("test_dag", job_type=None, translate=False)
+
+translate_dag = create_dag_own("translate_dag", job_type="sample", translate=True)
+
+
+globals()["sample_dag"] = sample_dag
+globals()["full_dag"] = full_dag
+globals()["test_dag"] = test_dag
+globals()["translate_dag"] = translate_dag
